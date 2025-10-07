5947f2575f4e1446c938a3799ccb63ca5a1f2172
ThamizselvanPsiog <thamizselvan.thanikainathan@psiog.com>
2025-10-07 17:46:39 +0530

    Added Main Program for Fragmentation

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1.Model;
+using ConsoleApp1.View;
+using ConsoleApp1.Controller;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParaController controller = new ParaController();
+
+            controller.Run();
+        }
+    }
+}
